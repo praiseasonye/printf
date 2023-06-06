a5af7e1680ccd2855f36536f58e7c2f2f0799391
praiseasonye <praiseikennaasonye@gmail.com>
2023-06-06 15:39:20 +0000

    testing changes to printf

--- print_func.c
+++ print_func.c
@@ -63,6 +63,9 @@ int string_printer(va_list args)
 
 int percent_printer(va_list args)
 {
+	char percent = '%';
+
 	(void)args;
-	return (write(1, "%%", 1));
+	return (write(1, &percent, 1));
+	return(0);
 }
