a0d828557205ce5300024d99f413da7923ddb435
praiseasonye <praiseikennaasonye@gmail.com>
2023-06-27 17:14:05 +0000

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- _printf.c
+++ _printf.c
@@ -18,8 +18,6 @@ int _printf(const char *format, ...)
 
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
 
 	va_start(args, format);
 	for (i = 0; format && format[i] != '\0'; i++)
